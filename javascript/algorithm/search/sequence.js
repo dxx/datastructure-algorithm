25e643d265c7e55ff1a477d6baa14164fc4aa553
code-mcx <1827939333@qq.com>
2020-10-29 18:17:24 +0800

    删除缩进 :file_folder:

--- javascript/algorithm/search/sequence.js
+++ javascript/algorithm/search/sequence.js
@@ -12,16 +12,16 @@ function sequenceSearch(nums, num) {
     }
   }
   return -1;
+}
+
+function main() {
+  let value = 8;
+  let nums = [2, 5, 1, 7, 8, 16];
+  let index = sequenceSearch(nums, value);
+  if (index != -1) {
+    console.log(value + " 在 nums 中的下标为: " + index);
   }
-  
-  function main() {
-    let value = 8;
-    let nums = [2, 5, 1, 7, 8, 16];
-    let index = sequenceSearch(nums, value);
-    if (index != -1) {
-      console.log(value + " 在 nums 中的下标为: " + index);
-    }
-  }
-  
-  main();
+}
+
+main();
   
