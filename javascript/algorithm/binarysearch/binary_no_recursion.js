825b4a8a65b7d6031a1c80ebb1d3af0fd4b617e5
code-mcx <1827939333@qq.com>
2020-11-04 08:22:37 +0800

    二分查找(非递归) :file_folder:

---
+++ javascript/algorithm/binarysearch/binary_no_recursion.js
@@ -0,0 +1,33 @@
+/**
+ * 二分法查找(非递归)
+ */
+
+function binarySearchNoRecursion(nums, findVal) {
+  let start = 0;
+  let end = nums.length - 1;
+  while (start <= end) {
+    let mid = (start + end) / 2;
+    if (findVal < nums[mid]) { // 查找的值在左边
+      end = mid - 1;
+    } else if (findVal > nums[mid]) { // 查找的值在右边
+      start = mid + 1;
+    } else {
+      // 找到目标值的下表
+      return mid;
+    }
+  }
+  return -1;
+}
+
+function main() {
+  let value = 100;
+  let nums = [1, 8, 10, 89, 100, 100, 123];
+  let index = binarySearchNoRecursion(nums, value);
+  if (index != -1) {
+    console.log("找到 " + value + ", 下标为 " + index);
+  } else {
+    console.log("未找到 " + value);
+  }
+}
+
+main();
