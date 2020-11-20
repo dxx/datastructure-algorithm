8e3e3aa9b8f40ad907c19e5495b1ace2f3fb2361
code-mcx <1827939333@qq.com>
2020-11-20 23:03:19 +0800

    代码修改 :file_folder:

--- golang/algorithm/greedy/set_covering.go
+++ golang/algorithm/greedy/set_covering.go
@@ -108,5 +108,5 @@ func main() {
         "B5": {"杭州", "大连"},
     }
     broadcasts := getBroadcast(broadcastMap)
-    fmt.Printf("最少选择的广播电台:%v", broadcasts)
+    fmt.Printf("最少选择的广播电台: %v", broadcasts)
 }
