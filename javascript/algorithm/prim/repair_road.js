303e3f71f8f0e5775f055f1a20fba688f6b0f25d
code-mcx <1827939333@qq.com>
2020-11-27 22:50:40 +0800

    普里姆(Prim)算法 :file_folder:

--- javascript/algorithm/prim/repair_road.js
+++ javascript/algorithm/prim/repair_road.js
@@ -9,6 +9,40 @@
  * 5.重复以上操作 n-1 次。
  */
 
+function MinTree(vertexes, edges) {
+  let numOfVertex = vertexes.length;
+  let initialVertexes = new Array(numOfVertex);
+  let initialMatrix = new Array(numOfVertex);
+  for (let i = 0; i < numOfVertex; i++) {
+    initialVertexes[i] = vertexes[i];
+    initialMatrix[i] = new Array(numOfVertex);
+    for (let j = 0; j < numOfVertex; j++) {
+      initialMatrix[i][j] = edges[i][j];
+    }
+  }
+  let graph = new Graph();
+  graph.vertexes = initialVertexes;
+  graph.matrix = initialMatrix;
+  this.graph = graph;
+}
+
+function Graph(vertexes, matrix) {
+  this.vertexes = vertexes; // 顶点
+  this.matrix = matrix; // 领接矩阵，代表边的值
+}
+
+MinTree.prototype.showGraph = function() {
+  for (let i = 0; i < this.graph.matrix.length; i++) {
+    let edges = this.graph.matrix[i];
+    let str = "[ ";
+    for (let j = 0; j < edges.length; j++) {
+      str += edges[j] + " ";
+    }
+    str += "]";
+    console.log(str);
+  }
+}
+
 function main() {
   let vertexes = ["A", "B", "C", "D", "E", "F", "G"];
   let edges = [
@@ -20,6 +54,8 @@ function main() {
     [Number.MAX_VALUE, Number.MAX_VALUE, Number.MAX_VALUE, 4, 5, Number.MAX_VALUE, 6],
     [2, 3, Number.MAX_VALUE, Number.MAX_VALUE, 4, 6, Number.MAX_VALUE]
   ];
+  let minTree = new MinTree(vertexes, edges);
+  minTree.showGraph();
 }
 
 main();
