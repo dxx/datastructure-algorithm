86725ac26f01a2148c4ebd649efccab0e513dbcf
dxx <dx1898@foxmail.com>
2026-06-12 00:32:36 +0800

    修改代码 📁

--- python/datastructure/stack/test_stack.py
+++ python/datastructure/stack/test_stack.py
@@ -7,7 +7,26 @@ class Test(unittest.TestCase):
     def test_stack(self):
         # 创建一个栈
         stack = Stack(3)
+        # 入栈
         stack.push("one")
         stack.push("two")
         stack.push("three")
+
+        # 栈满，无法入栈
+        is_success = stack.push("four")
+        if not is_success:
+            print("入栈失败!!!")
+        stack.show()
+
+        elem1 = stack.pop()
+        elem2 = stack.pop()
+        elem3 = stack.pop()
+
+        print("出栈: " + str(elem1))
+        print("出栈: " + str(elem2))
+        print("出栈: " + str(elem3))
+
+        elem = stack.pop()
+        if elem is None or elem == "":
+            print("出栈失败!!!")
         stack.show()
